3a350455f1b165d7d1d437622382740d905b46a2
Oguzhan <oguzhanturgut@outlook.com>
2020-03-16 22:38:50 +0100

    basic structure of adding todo

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet,Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const todoInputHandler = todoText => setTodo(todoText);
+  const addTodoHandler = () => setTodos(prevTodos =>  [...prevTodos, todo]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Add new todo..."
+                     style={styles.input}
+                     onChangeText={todoInputHandler}
+          />
+          <Button title="ADD" onPress={addTodoHandler}/>
+        </View>
+        <View>
+          {todos.map(todo => <Text key={todo}>{todo}</Text>)}
+        </View>
+      </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen:{
+    padding:50
   },
+  inputContainer:{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'},
+  input:{width: '85%', borderColor: 'black', borderWidth: 1, padding: 10}
 });
